9cf489839a16e4a3647fe1f4c6ba977ea5bbeb73
Matan Peled <chaosite@gmail.com>
2023-05-07 12:06:13 +0300

    Add some MethodToGraph tests

--- src/test/kotlin/il/ac/technion/cs/mipphd/graal/MethodToGraphTest.kt
+++ src/test/kotlin/il/ac/technion/cs/mipphd/graal/MethodToGraphTest.kt
@@ -3,10 +3,18 @@ package il.ac.technion.cs.mipphd.graal
 import il.ac.technion.cs.mipphd.graal.utils.MethodToGraph
 import il.ac.technion.cs.mipphd.graal.utils.SourcePosTool
 import org.junit.jupiter.api.Test
+import kotlin.reflect.jvm.javaMethod
 
 class AddTwoNumbers {
     fun add(x: Int, y: Int) = x+y
 }
+
+fun simpleFib(n: Int): Int =
+    try {
+    if(n == 0 || n == 1) { 1 } else {
+        simpleFib(n - 1) + simpleFib(n - 2)
+    }
+} catch(e: Throwable) { 0 }
 
 class Fib {
     var counter = 0L
@@ -26,10 +34,26 @@ class Fib {
 }
 
 internal class MethodToGraphTest {
-    val methodToGraph = MethodToGraph()
+    val methodToGraph = MethodToGraph(false)
     val addNumbersMethod = AddTwoNumbers::class.java.methods[0]
     val fibMethod = Fib::class.java.methods.find { it.name == "fib" }
 
+    @Test
+    fun `print graph of fib function`() {
+        println(methodToGraph.getAnalysisGraph(fibMethod).export())
+    }
+
+    @Test
+    fun `print graph of simpleFib function`() {
+        println(methodToGraph.getAnalysisGraph(::simpleFib.javaMethod).export())
+    }
+
+    @Test
+    fun `print sanitized graph of simpleFib function`() {
+        val graph = methodToGraph.getAnalysisGraph(::simpleFib.javaMethod)
+        graph.removeExceptions()
+        println(graph.export())
+    }
 
     @Test
     fun `get cfg does not throw exception`() {
